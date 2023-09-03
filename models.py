42a59844b4493ae5b673b517dd78f52401af0a80
Gaitano <gaitano.george@student.moringaschool.com>
2023-09-03 21:59:29 +0300

    Completed on the review model

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import  relationship, sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy import ForeignKey, Column, Integer, String, DateTime, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, DateTime, MetaData, desc
 
 engine = create_engine('sqlite:///restaurants.db')
 Base = declarative_base()
@@ -17,7 +17,26 @@ class Restaurant(Base):
     review = relationship('Review', back_populates='restaurant')
     
     def __repr__(self):
-        return f'Restaurant({self.id}: name="{self.name}", price={self.price})'    
+        return f'Restaurant({self.id}: name="{self.name}", price={self.price})'  
+    
+    @property
+    def restaurant_reviews(self):
+        return [review for review in self.reviews]  
+    
+    @property
+    def restaurant_customers(self):
+        return [review.customer for review in self.reviews]
+    
+    @classmethod
+    def fanciest(self):
+        fancy = session.query(Restaurant.name, Restaurant.price).order_by(desc(Restaurant.price)).first()
+        return fancy
+    
+    @property
+    def all_reviews():
+        sample = [restaurant for restaurant in session.query(Restaurant.restaurant_reviews).all()]
+        return sample
+    
 
 
 class Customer(Base):
@@ -33,6 +52,21 @@ class Customer(Base):
     def __repr__(self):
         return f'Customer(id{self.id}, first_name="{self.first_name}", last_name="{self.last_name}")'
     
+    @property
+    def customer_reviews(self):
+        return [review for review in self.reviews]
+    
+    @property
+    def customer_restaurants(self):
+        return [review.restaurant for review in self.reviews]
+    
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    # @property
+    # def favourite_restaurant(self):
+    #     best = 
+        
     
 class Review(Base):
     
@@ -49,7 +83,17 @@ class Review(Base):
     def __repr__(self):
         return f'Review(id={self.id}, star_rating={self.star_rating}, restaurant_id={self.restaurant_id}, customer_id={self.customer_id})'
     
+    @property
+    def customer(self):
+        return self.customer
     
+    @property
+    def restaurant(self):
+        return self.restaurant
+    
+    @property
+    def full_review(self):
+        return f'Review for{self.restaurant.name} by {self.customer.first_name} {self.customer.second_name}: {self.star_rating}'
     
 if __name__ == '__main__':
     
